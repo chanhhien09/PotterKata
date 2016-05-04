a43fa09bffebd6d47209e9913bf5a396dfce3f3b
saraisuu <chanhhien09@gmail.com>
2016-05-05 01:48:40 +0800

    First failed test - when buying one book, price is 100

---
+++ PotterKata.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace PotterKata.Tests
+{
+    [TestClass]
+    public class BooksPriceCalculatorTest
+    {
+        [TestMethod]
+        public void CalculateBookPrices_Book_1_Return_100()
+        {
+            BookPriceCalculator target = new BookPriceCalculator();
+            List<int> boughtBooks = new List<int>();
+            int expected = 100;
+
+            int actual = target.CalculatePrice(boughtBooks);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
